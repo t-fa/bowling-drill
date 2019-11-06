aedab4ec68266dbc4c98c3559e540ba712fb3ad7
t-fa <thomas.fattah@gmail.com>
2019-11-06 15:13:47 -0500

    Initial commit

--- app.js
+++ app.js
@@ -3,7 +3,13 @@ var express = require("express"),
               mongoose = require("mongoose");
 
 mongoose.set('useNewUrlParser',true);
-mongoose.connect("mongodb://localhost/bowling-drill");
+// mongoose.connect("mongodb://localhost/bowling-drill");
+
+app.set("view engine", "ejs");
+
+app.get("/", function(req, res){
+    res.redirect("index");
+})
 
 app.listen(3000, process.env.IP, function(){
     console.log("Server is running");
