d6d15541b89de82645c622779166e93ef4db861a
t-fa <thomas.fattah@gmail.com>
2019-11-07 17:42:28 -0500

    Added userSchema and beginning of RESTful routes

--- app.js
+++ app.js
@@ -1,15 +1,33 @@
 var express = require("express"),
               app = express(),
+              bodyParser = require("body-parser"),
               mongoose = require("mongoose");
 
 mongoose.set('useNewUrlParser',true);
-// mongoose.connect("mongodb://localhost/bowling-drill");
+mongoose.connect("mongodb://localhost/bowling-drill");
 
 app.use(express.static(__dirname + "/public"));
 app.set("view engine", "ejs");
 
+var userSchema = new mongoose.Schema({
+    firstname: String,
+    lastname: String,
+    email: String,
+    date: {type: Date, default: Date.now()}
+})
+
+var User = mongoose.model("User", userSchema);
+
+// var drillSchema = new mongoose.Schema({
+//     left
+// })
+
 app.get("/", function(req, res){
     res.render("index");
+})
+
+app.get("/drillings", function(req, res){
+    res.render("drillings");
 })
 
 app.listen(8080, process.env.IP, function(){
